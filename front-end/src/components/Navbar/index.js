fe0ed336615cdc3a3d459925966cf0375b02cb0e
Bakir-Rayan <rayanbak257@gmail.com>
2022-02-02 22:29:49 +0100

    building the navbar

---
+++ front-end/src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from "./NavbarElements";
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <h1>Logo</h1>
+                </NavLink>
+                <Bars />
+                <NavMenu>
+                    <NavLink to="/about" activeStyle>
+                        About
+                    </NavLink>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/Contact-us">Contact-us</NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
